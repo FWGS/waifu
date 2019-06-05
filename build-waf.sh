d91a43730538d3e606eba34d1bba603bf7045ce5
Alibek Omarov <a1ba.omarov@gmail.com>
2019-06-06 02:42:41 +0300

    build-waf: build two versions of waf, with and without waifu extensions

--- build-waf.sh
+++ build-waf.sh
@@ -3,8 +3,30 @@
 TOOLS="msvs,clang_compilation_database,color_msvc"
 PRELUDE=$'\tsys.path.insert(0, \'scripts/waifulib\')'
 
+# a set of relatively stable tools
+# TODO: make it possible to override this list
+WAIFU_TOOLS="gitversion,reconfigure,msdev,fwgslib,cxx11,force_32bit,subproject"
+
 pushd wafsrc
+echo "-- Building waf without waifu extensions: $TOOLS"
 ./waf-light "--tools=$TOOLS" "--prelude=$PRELUDE"
-mv waf ../
+mv waf ../waf-noext
 popd
 
+get_waifu_tools()
+{
+	OLD_IFS=$IFS
+	IFS=","
+	retval=""
+	for i in $WAIFU_TOOLS; do
+		retval="$retval,$PWD/scripts/waifulib/$i.py"
+	done
+	IFS=$OLD_IFS
+	echo "$retval"
+}
+TOOLS=$TOOLS$(get_waifu_tools)
+echo "-- Building waf with waifu extensions: $TOOLS"
+pushd wafsrc
+./waf-light "--tools=$TOOLS" "--prelude=$PRELUDE"
+mv waf ../waf-ext
+popd
