354a8e5eb14d60266578b4c4ac15d14f1afdd90e
Alibek Omarov <a1ba.omarov@gmail.com>
2019-11-05 00:30:22 +0300

    build-waf: remove msvcfix, include msvc_pdb instead. Add pthreads.py tool

--- build-waf.sh
+++ build-waf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TOOLS="msvs,clang_compilation_database,color_msvc"
-PRELUDE=$'\tContext.WAIFUVERSION=\'1.0.0\'\n\tsys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), \'scripts\', \'waifulib\'))'
+TOOLS="msvs,clang_compilation_database,color_msvc,msvc_pdb"
+PRELUDE=$'\tContext.WAIFUVERSION=\'1.1.0\'\n\tsys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), \'scripts\', \'waifulib\'))'
 
 # a set of relatively stable tools
 # TODO: make it possible to override this list
-WAIFU_TOOLS="gitversion,reconfigure,msdev,fwgslib,cxx11,force_32bit,subproject,strip_on_install,sdl2,msvcfix"
+WAIFU_TOOLS="gitversion,reconfigure,msdev,fwgslib,cxx11,force_32bit,subproject,strip_on_install,sdl2,pthreads"
 
 pushd wafsrc
 echo "-- Building waf without waifu extensions: $TOOLS"
