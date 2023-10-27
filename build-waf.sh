f0d1b04830baa8ad4a0d9f5edfa8c81bc425ef76
Alibek Omarov <a1ba.omarov@gmail.com>
2023-10-27 07:21:30 +0300

    build-waf: add gccdeps and msvcdeps

--- build-waf.sh
+++ build-waf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TOOLS="msvs,clang_compilation_database,color_msvc,msvc_pdb,pthread"
+TOOLS="msvs,clang_compilation_database,color_msvc,msvc_pdb,gccdeps,msvcdeps"
 PRELUDE=$'\tContext.WAIFUVERSION=\'1.1.0\'\n\tsys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), \'scripts\', \'waifulib\'))'
 
 # a set of relatively stable tools
 # TODO: make it possible to override this list
-WAIFU_TOOLS="gitversion,reconfigure,msdev,fwgslib,cxx11,force_32bit,subproject,strip_on_install,sdl2,enforce_pic"
+WAIFU_TOOLS="gitversion,reconfigure,msdev,fwgslib,cxx11,force_32bit,subproject,strip_on_install,sdl2,enforce_pic,pthread"
 
 pushd wafsrc
 echo "-- Building waf without waifu extensions: $TOOLS"
