3e44976fd3db1fd6431ea28c556f05afd4f127fa
Alibek Omarov <a1ba.omarov@gmail.com>
2024-05-25 03:47:41 +0300

    build-waf: release 1.3.0, drop the waifu name so ./waf --version won't be confusing, sign the binary by default

--- build-waf.sh
+++ build-waf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOOLS="msvs,clang_compilation_database,color_msvc,msvc_pdb,gccdeps,msvcdeps,pthread"
-PRELUDE=$'\tContext.WAIFUVERSION=\'1.2.0\'\n\tsys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), \'scripts\', \'waifulib\'))'
+PRELUDE=$'\tfrom waflib import Context\n\tContext.WAIFUVERSION=\'1.3.0\'\n\tsys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), \'scripts\', \'waifulib\'))'
 
 # a set of relatively stable tools
 # TODO: make it possible to override this list
@@ -9,7 +9,7 @@ WAIFU_TOOLS="gitversion,reconfigure,msdev,fwgslib,cxx11,force_32bit,subproject,s
 
 pushd wafsrc
 echo "-- Building waf without waifu extensions: $TOOLS"
-./waf-light "--tools=$TOOLS" "--prelude=$PRELUDE" "--set-name=waifu" --nostrip
+./waf-light "--tools=$TOOLS" "--prelude=$PRELUDE" --nostrip --sign
 mv waf ../waf-noext
 popd
 
@@ -27,6 +27,6 @@ get_waifu_tools()
 TOOLS=$TOOLS$(get_waifu_tools)
 echo "-- Building waf with waifu extensions: $TOOLS"
 pushd wafsrc
-./waf-light "--tools=$TOOLS" "--prelude=$PRELUDE" "--set-name=waifu" --nostrip
+./waf-light "--tools=$TOOLS" "--prelude=$PRELUDE" --nostrip --sign
 mv waf ../waf-ext
 popd
